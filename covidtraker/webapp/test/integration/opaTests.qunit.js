8f484d86556500dc258cf0fdafb0a5b3dbd2f6e3
devraj <dev.raj@capgemini.com>
2023-03-28 19:56:28 +0530

    covid traker

---
+++ covidtraker/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["Traker/covidtraker/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ covidtraker/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"Traker/covidtraker/test/unit/controller/Home.controller"
+], function () {
+	"use strict";
+});
